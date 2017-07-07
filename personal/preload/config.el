2da290016b5db690e2baa435d27ea652de3bdd40
Axel Gagge <axel.gagge@gmail.com>
2017-07-07 10:28:39 +0200

    Added a first attempt at config

---
+++ personal/preload/config.el
@@ -0,0 +1,6 @@
+;; Personal emacs config
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+(global-set-key (kbd "C-c =") 'eval-region)
+(global-set-key (kbd "C-c #") 'comment-or-uncomment-region)
